fed4daa6d85ef8890d99a7530eba22436f342c2e
Branson <bransonneo@gmail.com>
2026-06-10 08:17:45 +0000

    Now has a score counter and food spawns

--- code.js
+++ code.js
@@ -3,6 +3,8 @@ const ctx = canvas.getContext("2d");
 
 const box = 20;
 
+let score = 0;
+
 let snake = [
   { x: 500,
     y: 300 }
@@ -25,14 +27,39 @@ function draw() {
     y: snake[0].y + dy
   };
 
-  snake.unshift(newHead);
+  for (let i = 0; i < snake.length; i++) {
+    if (newHead.x === snake[i].x && newHead.y === snake[i].y) {
+      clearInterval(game);
+      alert(`You bumped into yourself! Game Over! Your score: ${score}`);
+      return;
+    }
+  }
 
+let ateFood = (newHead.x === food.x && newHead.y === food.y);
+
+snake.unshift(newHead);
+
+if (ateFood) {
+  score++;
+
+  food = {
+    x: Math.floor(Math.random() * (1000 / box)) * box,
+    y: Math.floor(Math.random() * (600 / box)) * box
+  };
+
+} else {
   snake.pop();
+}
+  
 
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, 1000, 600);
 
   ctx.fillStyle = "white";
+  
+  ctx.fillStyle = "white";
+  ctx.font = "24px Arial";
+  ctx.fillText("Score: " + score, 20, 30);
 
   for (let i = 0; i < snake.length; i++) {
     ctx.fillRect(
@@ -43,17 +70,28 @@ function draw() {
     );
   }
 
+  ctx.fillStyle = "red";
 
-  let head = snake[0];
+  ctx.beginPath();
+
+  ctx.arc(
+    food.x + box/2,
+    food.y + box/2,
+    box/2,
+    0,
+    Math.PI * 2
+  );
+
+  ctx.fill();
 
   if (
-    head.x < 0 ||
-    head.x >= 1000 ||
-    head.y < 0 ||
-    head.y >= 600
+    newHead.x < 0 ||
+    newHead.x >= 1000 ||
+    newHead.y < 0 ||
+    newHead.y >= 600
   ) {
     clearInterval(game);
-    alert("Game Over!");
+    alert(`You bumped into the wall. Game Over! Your score: ${score}`);
   }
 }
 
